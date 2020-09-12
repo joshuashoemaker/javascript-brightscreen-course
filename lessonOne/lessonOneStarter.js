40513da687ef8c3a3c4b73055feb2fc3de0f3f83
joshuashoemaker <joshuashoemaker@outlook.com>
2020-09-12 17:09:35 -0500

    🚀 feat: added start file to lesson one

---
+++ lessonOne/lessonOneStarter.js
@@ -0,0 +1,9 @@
+/*
+  The purpose of this test is to create a funciton
+  that returns the value passed to it. Use the code
+  below as a template for the function.
+*/
+
+function (props) {
+  return ''
+}
